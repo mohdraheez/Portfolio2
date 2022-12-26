a6c52fff7df668fe5609007fac7589778825c290
Mohammed Raheez <42299547+mohdraheez@users.noreply.github.com>
2022-12-27 01:41:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 var body = document.querySelector('body');
 var box = document.querySelector('.box');
 var hr0 = document.querySelector('.hr0');
-var hr1 =  document.querySelector('.hr1');
+var hr1 = document.querySelector('.hr1');
 var hr2 = document.querySelector('.hr2');
 var hr3 = document.querySelector('.hr3');
 var hr4 = document.querySelector('.hr4');
-var h =  document.querySelector('.h');
+var h = document.querySelector('.h');
 var home = document.querySelector('#home');
 var aboutdiv = document.querySelector('.aboutdiv');
-var skillsdiv =  document.querySelector('.skillsdiv');
+var skillsdiv = document.querySelector('.skillsdiv');
 var about = document.querySelector('#about');
 var projectdiv = document.querySelector('.projectdiv');
 var skills = document.querySelector('#skills');
 var contactdiv = document.querySelector('.contactdiv');
 var projects = document.querySelector('#projects');
 var contact = document.querySelector('#contact');
+
+window.addEventListener('hashchange', function (e) {
+    var loc = location.hash;
+    if(loc==='#home'){
+        h.classList.add('i');
+    }
+    else{
+        h.classList.remove('i');
+    }
+});
+
+loader();
 
 body.addEventListener('pointermove', (e) => {
 
@@ -55,51 +67,27 @@ let touchstartY = 0
 let touchendY = 0
 var flag = 0;
 
-hr0.classList.add('hrx');
-h.classList.add('i');        
-addContents();
-home.classList.add('fadein');
-
-function addContents(){
-    aboutdiv.innerHTML = home.innerHTML;
-    skillsdiv.innerHTML = about.innerHTML;
-    projectdiv.innerHTML = skills.innerHTML;
-    contactdiv.innerHTML = projects.innerHTML;
-}
-
-function removeContent(){
-    aboutdiv.innerHTML = "";
-   skillsdiv.innerHTML = "";
-    projectdiv.innerHTML = "";
-    contactdiv.innerHTML = "";
-}
-
-setTimeout(() => {
-    home.classList.remove('fadein');
-}, 1000)
-
-
 function wheeldetector(e) {
 
-        if (e.deltaY > 110) {
-            if(flag===0){
-                swipedup();
-                flag=1;
-                setTimeout(()=>{
-                    flag=0;
-                },200);
-            }
+    if (e.deltaY > 110) {
+        if (flag === 0) {
+            swipedup();
+            flag = 1;
+            setTimeout(() => {
+                flag = 0;
+            }, 200);
+        }
 
+    }
+    else if (e.deltaY < -110) {
+        if (flag === 0) {
+            swipeddown();
+            flag = 1;
+            setTimeout(() => {
+                flag = 0;
+            }, 200);
         }
-        else if (e.deltaY < -110) {
-            if(flag===0){
-                swipeddown();
-                flag=1;
-                setTimeout(()=>{
-                    flag=0;
-                },200);
-            }
-        }
+    }
 }
 
 document.addEventListener('wheel', wheeldetector);
@@ -125,29 +113,30 @@ document.addEventListener('touchend', e => {
     checkDirection()
 })
 
-var keyflag=0;
+var keyflag = 0;
 document.addEventListener('keydown', (e) => {
-    if(keyflag===0){
+    if (keyflag === 0) {
         if (e.which === 40) {
             swipedup();
         }
-    
+
         if (e.which === 38) {
             swipeddown();
         }
         keyflag = 1;
     }
 
-    setTimeout(()=>{
+    setTimeout(() => {
         keyflag = 0;
-    },300);
+    }, 300);
 })
 
 
 function swipedup() {
-    addContents();
     var loc = window.location.hash;
+
     if (loc === '' || loc === '#home') {
+        aboutdiv.innerHTML = home.innerHTML;
         location.href = '#about';
         about.classList.add('fadein');
         hr1.classList.add('hrx');
@@ -155,49 +144,56 @@ function swipedup() {
         h.classList.remove('i');
         aboutdiv.classList.add('fadeoutabout');
         setTimeout(() => {
+            aboutdiv.innerHTML = "";
             about.classList.remove('fadein');
             aboutdiv.classList.remove('fadeoutabout');
             aboutdiv.style.visibility = 'hidden';
         }, 1000)
     }
     if (loc === '#about') {
+        skillsdiv.innerHTML = about.innerHTML;
         location.href = '#skills';
         skills.classList.add('fadein');
-       hr1.classList.remove('hrx');
+        hr1.classList.remove('hrx');
         hr2.classList.add('hrx');
-       skillsdiv.classList.add('fadeoutskills');
+        skillsdiv.classList.add('fadeoutskills');
         setTimeout(() => {
-           skillsdiv.classList.remove('fadeoutskills');
-           skillsdiv.style.visibility = 'hidden';
+            skillsdiv.innerHTML = "";
+            skillsdiv.classList.remove('fadeoutskills');
+            skillsdiv.style.visibility = 'hidden';
             skills.classList.remove('fadein');
         }, 1000)
     }
     if (loc === '#skills') {
+        projectdiv.innerHTML = skills.innerHTML;
         location.href = '#projects';
         projects.classList.add('fadein');
         hr2.classList.remove('hrx');
         hr3.classList.add('hrx');
         projectdiv.classList.add('fadeoutprojects');
         setTimeout(() => {
+            projectdiv.innerHTML = "";
             projectdiv.style.visibility = 'hidden';
             projects.classList.remove('fadein');
             projectdiv.classList.remove('fadeoutprojects');
         }, 1000)
     }
     if (loc === '#projects') {
+        contactdiv.innerHTML = projects.innerHTML;
         location.href = '#contact';
         contact.classList.add('fadein');
         hr3.classList.remove('hrx');
         hr4.classList.add('hrx');
         contactdiv.classList.add('fadeoutcontact');
         setTimeout(() => {
+            contactdiv.innerHTML = "";
             contactdiv.style.visibility = 'hidden';
             contact.classList.remove('fadein');
             contactdiv.classList.remove('fadeoutcontact');
         }, 1000)
     }
 
-    if(loc==='#contact'){
+    if (loc === '#contact') {
         contactdiv.style.visibility = 'hidden';
     }
 }
@@ -205,12 +201,11 @@ function swipedup() {
 
 function swipeddown() {
     var loc = window.location.hash;
-    removeContent();
     if (loc === '#about') {
         location.href = '#home';
         home.classList.add('fadein');
         hr0.classList.add('hrx');
-       hr1.classList.remove('hrx');
+        hr1.classList.remove('hrx');
         h.classList.remove('i');
         h.classList.add('i');
         aboutdiv.style.visibility = 'visible';
@@ -220,10 +215,10 @@ function swipeddown() {
     }
     if (loc === '#skills') {
         location.href = '#about';
-         about.classList.add('fadein');
-       hr1.classList.add('hrx');
+        about.classList.add('fadein');
+        hr1.classList.add('hrx');
         hr2.classList.remove('hrx');
-       skillsdiv.style.visibility = 'visible';
+        skillsdiv.style.visibility = 'visible';
         setTimeout(() => {
             about.classList.remove('fadein');
         }, 1000)
@@ -249,79 +244,39 @@ function swipeddown() {
         }, 1000)
     }
 }
-document.addEventListener('mousemove', function (e) {
-    if(e.clientX>120 && e.clientX<500){
-     var imgs = [
-         'r.png',
-         'html.png',
-         'css.png',
-         'flower.png',
-         'js.png'
-     ]
 
-     var rand = Math.floor(Math.random() * 5);
-     let div = document.createElement('div');
-     div.classList.add('div1');
-     let star = document.createElement('img');
-     star.classList.add('star');
-     star.setAttribute("name", "star");
-     star.setAttribute("src", "public/" + imgs[rand]);
-     let x = e.offsetX;
-     let y = e.offsetY;
-     
-     div.style.left = x + 'px';
-     div.style.top = y + 'px';
- 
-     let size = Math.random() * 20;
- 
-     star.style.width = 20 + size + 'px';
-     star.style.height = 20 + size + 'px';
- 
- 
-     let transformValue = Math.random() * 360;
-     div.style.transform = 'rotate(' + transformValue + 'deg)';
- 
-     div.appendChild(star);
-     body.appendChild(div);
- 
-     setTimeout(function () {
-         star.remove();
-     }, 500)
-    }
- })
-
- document.querySelector('.open').addEventListener('click',()=>{
-    if(document.querySelector('.btnimg').getAttribute('src')==='public/bar.png'){
-        document.querySelector('.btnimg').setAttribute('src','public/x.png')
-        for(var i=0;i<5;i++){
-            document.querySelector('.b'+(i+1)).classList.add('btn');
+document.querySelector('.open').addEventListener('click', () => {
+    if (document.querySelector('.btnimg').getAttribute('src') === 'public/bar.png') {
+        document.querySelector('.btnimg').setAttribute('src', 'public/x.png')
+        for (var i = 0; i < 5; i++) {
+            document.querySelector('.b' + (i + 1)).classList.add('btn');
         }
     }
-    else{
-        document.querySelector('.btnimg').setAttribute('src','public/bar.png')
-        
-            for(var i=0;i<5;i++){
-            document.querySelector('.b'+(i+1)).classList.add('btnout');
+    else {
+        document.querySelector('.btnimg').setAttribute('src', 'public/bar.png')
+
+        for (var i = 0; i < 5; i++) {
+            document.querySelector('.b' + (i + 1)).classList.add('btnout');
+        }
+
+        setTimeout(() => {
+            for (var i = 0; i < 5; i++) {
+                document.querySelector('.b' + (i + 1)).classList.remove('btn');
+                document.querySelector('.b' + (i + 1)).classList.remove('btnout');
             }
-            
-            setTimeout(()=>{
-                for(var i=0;i<5;i++){
-                document.querySelector('.b'+(i+1)).classList.remove('btn');
-                document.querySelector('.b'+(i+1)).classList.remove('btnout');
-                }
-            },950)
-        
+        }, 100)
+
     }
 })
 
 
-for(var i=0;i<5;i++)
-    document.querySelector('.b'+(i+1)).addEventListener('click',(e)=>{ 
+for (var i = 0; i < 5; i++)
+    document.querySelector('.b' + (i + 1)).addEventListener('click', (e) => {
         switchpage(e.target.classList[0]);
-});
+    });
 
 
-function removehrx(){
+function removehrx() {
     hr0.classList.remove('hrx');
     hr1.classList.remove('hrx');
     hr2.classList.remove('hrx');
@@ -329,12 +284,11 @@ function removehrx(){
     hr4.classList.remove('hrx');
 }
 
-function switchpage(page){
+function switchpage(page) {
 
-    removeContent();
     removehrx();
 
-    switch(page){
+    switch (page) {
         case 'b1':
             location.href = '#home';
             home.classList.add('fadein');
@@ -342,21 +296,21 @@ function switchpage(page){
             h.classList.remove('i');
             h.classList.add('i');
 
-            setTimeout(()=>{
+            setTimeout(() => {
                 home.classList.remove('fadein');
-            },1000);
+            }, 1000);
 
-        break;
+            break;
 
         case 'b2':
             location.href = '#about';
             about.classList.add('fadein');
-           hr1.classList.add('hrx');
+            hr1.classList.add('hrx');
             h.classList.remove('i');
-            setTimeout(()=>{
+            setTimeout(() => {
                 about.classList.remove('fadein');
-            },1000);
-        break;
+            }, 1000);
+            break;
 
         case 'b3':
             location.href = '#skills';
@@ -365,8 +319,8 @@ function switchpage(page){
             h.classList.remove('i');
             setTimeout(() => {
                 skills.classList.remove('fadein');
-            },1000)
-        break;
+            }, 1000)
+            break;
 
         case 'b4':
             location.href = '#projects';
@@ -375,8 +329,8 @@ function switchpage(page){
             h.classList.remove('i');
             setTimeout(() => {
                 projects.classList.remove('fadein');
-            },1000)
-        break;
+            }, 1000)
+            break;
 
         case 'b5':
             location.href = '#contact';
@@ -385,8 +339,8 @@ function switchpage(page){
             h.classList.remove('i');
             setTimeout(() => {
                 contact.classList.remove('fadein');
-            },1000)
-        break;
+            }, 1000)
+            break;
 
 
     }
@@ -394,6 +348,65 @@ function switchpage(page){
 }
 
 
-function loadproject(){
+
+function loadproject() {
     location.href = 'project.html'
+}
+
+function loader() {
+    var loc = window.location.hash;
+
+    if (loc === '' || loc === '#home') {
+        hr0.classList.add('hrx');
+        h.classList.add('i');
+        home.classList.add('fadein');
+        setTimeout(() => {
+            home.classList.remove('fadein');
+        }, 1000)
+
+        console.log('added home')
+    }
+
+    if (loc === '#about') {
+        hr1.classList.add('hrx');
+        about.classList.add('fadein');
+        setTimeout(() => {
+            about.classList.remove('fadein');
+        }, 1000)
+
+        console.log('added about')
+
+    }
+
+    if (loc === '#skills') {
+        hr2.classList.add('hrx');
+        skills.classList.add('fadein');
+        setTimeout(() => {
+            skills.classList.remove('fadein');
+        }, 1000)
+
+        console.log('added skills')
+
+    }
+
+    if (loc === '#projects') {
+        hr3.classList.add('hrx');
+        projects.classList.add('fadein');
+        setTimeout(() => {
+            projects.classList.remove('fadein');
+        }, 1000)
+
+        console.log('added project')
+
+    }
+
+    if (loc === '#contact') {
+        hr4.classList.add('hrx');
+        contact.classList.add('fadein');
+        setTimeout(() => {
+            contact.classList.remove('fadein');
+        }, 1000)
+        console.log('added contact')
+
+    }
 }
